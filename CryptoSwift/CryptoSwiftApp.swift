56f7242fb1ad4ca639a7d4a84ace1066ef764f62
Nortiz <nortiz1349@gmail.com>
2022-10-18 14:40:08 +0900

    Initial Commit

---
+++ CryptoSwift/CryptoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSwiftApp.swift
+//  CryptoSwift
+//
+//  Created by Nortiz M1 on 2022/10/18.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
