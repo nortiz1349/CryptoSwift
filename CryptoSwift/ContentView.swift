c792dcadb9e770f93f46b3b1a7922080f24c7ca8
Nortiz <nortiz1349@gmail.com>
2022-10-18 15:02:53 +0900

    Project set up and custom color theme

--- CryptoSwift/ContentView.swift
+++ CryptoSwift/ContentView.swift
@@ -4,18 +4,29 @@
 //
 //  Created by Nortiz M1 on 2022/10/18.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+		ZStack {
+			Color.theme.background.ignoresSafeArea()
+			
+			VStack {
+				Text("Accent Color")
+					.foregroundColor(.theme.accent)
+				
+				Text("Secondary Text Color")
+					.foregroundColor(.theme.secondaryText)
+				
+				Text("Red Color")
+					.foregroundColor(.theme.red)
+				
+				Text("Green Color")
+					.foregroundColor(.theme.green)
+			}
+			.font(.title)
+			.bold()
+		}
     }
 }
 
