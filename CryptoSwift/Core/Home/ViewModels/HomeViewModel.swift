8bceafaaed14ec67b6289825bee178de274a10e5
Nortiz <nortiz1349@gmail.com>
2022-10-20 12:22:46 +0900

    filtering based on search

--- CryptoSwift/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoSwift/Core/Home/ViewModels/HomeViewModel.swift
@@ -24,11 +24,30 @@ class HomeViewModel: ObservableObject {
 	}
 	
 	func addSubscriber() {
-		dataService.$allCoins
-			.sink { [weak self] returnedCoins in
-				self?.allCoins = returnedCoins
+		
+		// updates allCoins
+		$searchText
+			.combineLatest(dataService.$allCoins)
+			.debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+			.map(filterCoins)
+			.sink { [weak self] returnedCoin in
+				self?.allCoins = returnedCoin
 			}
 			.store(in: &cancellables)
+	}
+	
+	private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+		guard !text.isEmpty else {
+			return coins
+		}
+		
+		let lowercasedText = text.lowercased()
+		
+		return coins.filter { coin in
+			return coin.name.lowercased().contains(lowercasedText) ||
+			coin.symbol.lowercased().contains(lowercasedText) ||
+			coin.id.lowercased().contains(lowercasedText)
+		}
 	}
 	
 }
