138d374f06bbf107a0229a82250586519e307ad6
Nortiz <nortiz1349@gmail.com>
2022-10-20 11:07:05 +0900

    Saving Image to the File Manager

--- CryptoSwift/Extensions/Double.swift
+++ CryptoSwift/Extensions/Double.swift
@@ -17,9 +17,9 @@ extension Double {
 		let formatter = NumberFormatter()
 		formatter.usesGroupingSeparator = true
 		formatter.numberStyle = .currency
-		//formatter.locale = .current // <- default value
-		//formatter.currencyCode = "usd" // <- change currency
-		//formatter.currencySymbol = "$" // <- change currency symbol
+//		formatter.locale = .current // <- default value
+		formatter.currencyCode = "usd" // <- change currency
+		formatter.currencySymbol = "$" // <- change currency symbol
 		formatter.minimumFractionDigits = 2
 		formatter.maximumFractionDigits = 2
 		return formatter
@@ -45,8 +45,8 @@ extension Double {
 		formatter.usesGroupingSeparator = true
 		formatter.numberStyle = .currency
 		//formatter.locale = .current // <- default value
-		//formatter.currencyCode = "usd" // <- change currency
-		//formatter.currencySymbol = "$" // <- change currency symbol
+		formatter.currencyCode = "usd" // <- change currency
+		formatter.currencySymbol = "$" // <- change currency symbol
 		formatter.minimumFractionDigits = 2
 		formatter.maximumFractionDigits = 6
 		return formatter
